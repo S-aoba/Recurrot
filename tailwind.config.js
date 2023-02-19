ba0902bfb1354e7347c9c3e8741e60a153ebc26b
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-02-19 13:03:54 +0900

    🎨 tailwindのresponsive screensをmantineに合わせた

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      "xs":	"576",
+      "sm"	:"768",
+      "md"	:"992",
+      "lg"	:"1200",
+      "xl"	:"1400"
+    }
   },
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: [require('prettier-plugin-tailwindcss'),require('@tailwindcss/line-clamp'),],
 }
