ea1e6b65abc22601977c4b58b63c7ca3d4e5ef02
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-02-21 14:22:43 +0900

    ♻️ Change: pages/index.tsx className

--- src/pages/questions/post/index.tsx
+++ src/pages/questions/post/index.tsx
@@ -2,7 +2,7 @@ import { Form } from '@/component/Form'
 
 const QuestionPost = () => {
   return (
-    <main className=' flex h-fit flex-col items-center gap-y-10 p-5'>
+    <main className=' flex h-fit min-h-full justify-center p-5'>
       <Form />
     </main>
   )
