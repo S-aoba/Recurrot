93fa1a234af9e5f2187451129de9027a2c135c30
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-02-25 22:43:35 +0900

    🔥 Rm:css

--- src/component/Layout/Navigation.tsx
+++ src/component/Layout/Navigation.tsx
@@ -1,6 +1,6 @@
 export const Navigation = () => {
   return (
-    <div className=' col-span-2 flex justify-center'>
+    <div className=' flex justify-center'>
       <nav className=' w-full'>
         <div className=' flex w-full justify-center border-t-0 border-l-0 border-r-0 border-b border-solid border-gray-200'>
           <ul className=' flex w-9/12 list-none gap-x-5 pl-0 text-sm'>
