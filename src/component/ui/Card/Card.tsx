4ff99ce33d29e0fc6129e92b33e25886767bc3ea
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-02 16:49:50 +0900

    ♻️ dummyQuestionItemにquestionIdを追記

--- src/component/ui/Card/Card.tsx
+++ src/component/ui/Card/Card.tsx
@@ -13,10 +13,10 @@ export const Card = () => {
       {dummyQuestionItemList.map((dummyQuestionItem) => {
         return (
           <div
-            key={dummyQuestionItem.userName}
+            key={dummyQuestionItem.questionId}
             className=' col-span-1 h-64 w-80 border border-solid border-gray-300 bg-white'
           >
-            <Link href={'/questions/1'} className=' absolute block h-64 w-80'></Link>
+            <Link href={`questions/${dummyQuestionItem.questionId}`} className=' absolute block h-64 w-80'></Link>
             <div className=' px row-span-2 flex items-center'>
               <div className=' p-5'>
                 <Image src={'/typescript.png'} height={95} width={95} alt={'typescript'} className=' rounded-xl' />
@@ -46,56 +46,83 @@ export const Card = () => {
 }
 
 type DummyQuestionItemType = {
+  questionId: string
   userName: string
   postedDate: string
   questionTitle: string
+  questionContent: string
   replayCount: number
 }
 
 const dummyQuestionItemList: DummyQuestionItemType[] = [
   {
+    questionId: '0',
     userName: 'Aoba',
     postedDate: '2023/1/18',
     questionTitle:
       'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
+    questionContent:
+      'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
     replayCount: 5,
   },
   {
+    questionId: '1',
     userName: 'test10',
     postedDate: '2023/1/19',
     questionTitle:
       'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
+    questionContent:
+      'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
     replayCount: 2,
   },
   {
+    questionId: '2',
     userName: 'test0',
     postedDate: '2023/1/19',
     questionTitle:
       'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
+    questionContent:
+      'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
     replayCount: 4,
   },
   {
+    questionId: '3',
+
     userName: 'test1',
     postedDate: '2023/1/19',
     questionTitle: 'この文章はダミーです。文字の大きさ、量、字間、行間',
+    questionContent:
+      'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
     replayCount: 4,
   },
   {
+    questionId: '4',
+
     userName: 'test2',
     postedDate: '2023/1/19',
     questionTitle: 'この文章はダミーです。文字の大きさ、量、字間、行間',
+    questionContent:
+      'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
     replayCount: 4,
   },
   {
+    questionId: '5',
+
     userName: 'test3',
     postedDate: '2023/1/19',
     questionTitle: 'この文章はダミーです。文字の大きさ、量、字間、行間',
+    questionContent:
+      'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
     replayCount: 4,
   },
   {
+    questionId: '6',
+
     userName: 'test4',
     postedDate: '2023/1/19',
     questionTitle:
+      'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
+    questionContent:
       'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
     replayCount: 4,
   },
