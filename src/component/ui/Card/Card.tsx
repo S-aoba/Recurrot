d329a26750f4eae04c46810533f9689e645eeba9
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-02 16:42:54 +0900

    🩹 DummyDataの名称変更

--- src/component/ui/Card/Card.tsx
+++ src/component/ui/Card/Card.tsx
@@ -10,33 +10,33 @@ import Link from 'next/link'
 export const Card = () => {
   return (
     <>
-      {dummyUserDataArray.map((dummyUserData) => {
+      {dummyQuestionItemList.map((dummyQuestionItem) => {
         return (
           <div
-            key={dummyUserData.userName}
+            key={dummyQuestionItem.userName}
             className=' col-span-1 h-64 w-80 border border-solid border-gray-300 bg-white'
           >
             <Link href={'/questions/1'} className=' absolute block h-64 w-80'></Link>
-            <div className=' px row-span-2 flex items-center bg-red-500'>
+            <div className=' px row-span-2 flex items-center'>
               <div className=' p-5'>
                 <Image src={'/typescript.png'} height={95} width={95} alt={'typescript'} className=' rounded-xl' />
               </div>
               <div className='w-full p-2'>
                 <div className=' flex w-full items-center gap-x-3 py-3'>
                   <Avatar radius='xl' />
-                  <span>{dummyUserData.userName}</span>
+                  <span>{dummyQuestionItem.userName}</span>
                 </div>
                 <div className=' flex w-full items-center gap-x-3 pb-3'>
-                  <span>{dummyUserData.postedDate}</span>
+                  <span>{dummyQuestionItem.postedDate}</span>
                   <div className=' flex items-center gap-x-1'>
                     <IconMessageCircle2 />
-                    <span>{dummyUserData.replayCount}</span>
+                    <span>{dummyQuestionItem.replayCount}</span>
                   </div>
                 </div>
               </div>
             </div>
             <div className=' row-span-4 p-3 text-lg'>
-              <p className=' line-clamp-3'>{dummyUserData.questionTitle}</p>
+              <p className=' line-clamp-3'>{dummyQuestionItem.questionTitle}</p>
             </div>
           </div>
         )
@@ -45,14 +45,14 @@ export const Card = () => {
   )
 }
 
-type DummyUser = {
+type DummyQuestionItemType = {
   userName: string
   postedDate: string
   questionTitle: string
   replayCount: number
 }
 
-const dummyUserDataArray: DummyUser[] = [
+const dummyQuestionItemList: DummyQuestionItemType[] = [
   {
     userName: 'Aoba',
     postedDate: '2023/1/18',
@@ -61,41 +61,42 @@ const dummyUserDataArray: DummyUser[] = [
     replayCount: 5,
   },
   {
-    userName: '美紀子',
+    userName: 'test10',
     postedDate: '2023/1/19',
     questionTitle:
       'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
     replayCount: 2,
   },
   {
-    userName: 'hikari',
+    userName: 'test0',
     postedDate: '2023/1/19',
     questionTitle:
       'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
     replayCount: 4,
   },
   {
-    userName: 'hikern',
+    userName: 'test1',
     postedDate: '2023/1/19',
     questionTitle: 'この文章はダミーです。文字の大きさ、量、字間、行間',
     replayCount: 4,
   },
   {
-    userName: 'hikern',
+    userName: 'test2',
     postedDate: '2023/1/19',
     questionTitle: 'この文章はダミーです。文字の大きさ、量、字間、行間',
     replayCount: 4,
   },
   {
-    userName: 'hikern',
+    userName: 'test3',
     postedDate: '2023/1/19',
     questionTitle: 'この文章はダミーです。文字の大きさ、量、字間、行間',
     replayCount: 4,
   },
   {
-    userName: 'hikern',
+    userName: 'test4',
     postedDate: '2023/1/19',
-    questionTitle: 'この文章はダミーです。文字の大きさ、量、字間、行間',
+    questionTitle:
+      'この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ',
     replayCount: 4,
   },
 ]
