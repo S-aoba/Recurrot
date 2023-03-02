cd4aff5eb4af0d230a639b9e02620949032bf8cd
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-02 12:08:27 +0900

    ♻️ Navigationのコンポーネント化

--- src/component/ui/Navigation/Navigation.tsx
+++ src/component/ui/Navigation/Navigation.tsx
@@ -1,3 +1,6 @@
+import { MainNavTab } from './MainNavTab'
+import { SubNavTab } from './SubNavTab'
+
 /**
  * @package
  */
@@ -7,18 +10,14 @@ export const Navigation = () => {
     <div className=' flex justify-center'>
       <nav className=' w-full'>
         <div className=' flex w-full justify-center border-t-0 border-l-0 border-r-0 border-b border-solid border-gray-200'>
-          <ul className=' flex w-9/12 list-none gap-x-5 pl-0 text-sm'>
-            <li>質問</li>
-            <li>イベント</li>
-            <li>お知らせ</li>
-            <li>ダッシュボード</li>
-          </ul>
+          <div className=' flex w-9/12 list-none gap-x-5 py-3 text-sm'>
+            <MainNavTab />
+          </div>
         </div>
         <div className=' flex w-full justify-center border-t-0 border-l-0 border-r-0 border-b border-solid border-gray-200'>
-          <ul className=' flex w-9/12 list-none gap-x-5 pl-0 text-sm'>
-            <li>新着</li>
-            <li>回答募集中</li>
-          </ul>
+          <div className=' flex w-9/12 list-none gap-x-5 py-3 text-sm'>
+            <SubNavTab />
+          </div>
         </div>
       </nav>
     </div>
