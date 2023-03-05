2965146856abe6eef6e1938250e61b3249ef3d04
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-05 22:01:04 +0900

    🏷️ add:QuestionType HashtagType

--- src/common/type/index.tsx
+++ src/common/type/index.tsx
@@ -5,3 +5,17 @@ export type MainUrlVal = (typeof MAIN_URL)[MainUrlKeys]
 
 export type SubUrlKeys = keyof typeof SUB_URL
 export type SubUrlVal = (typeof SUB_URL)[SubUrlKeys]
+
+export type QuestionType = {
+  questionId: string
+  postedUseId: string
+  title: string
+  content: string
+  postDate: string
+  hashtagList: HashtagType[]
+  answerList: string[]
+}
+
+export type HashtagType = {
+  hashtag: string
+}
