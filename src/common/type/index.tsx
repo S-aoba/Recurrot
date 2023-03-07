be68fe74e7ff9baf4e12ded7634803b9672aeda2
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-07 21:32:46 +0900

    🚚 constをcommonフォルダに移動

--- src/common/type/index.tsx
+++ src/common/type/index.tsx
@@ -1,4 +1,4 @@
-import type { MAIN_URL, SUB_URL } from '../../component/const'
+import type { MAIN_URL, SUB_URL } from '../const'
 
 export type MainUrlKeys = keyof typeof MAIN_URL
 export type MainUrlVal = (typeof MAIN_URL)[MainUrlKeys]
