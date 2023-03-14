d21a7be74934bbe6add01385cbbba4f3b73393a0
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-14 18:46:18 +0900

    💬 add:queryClient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,24 @@
 import '../../styles/globals.css'
 
 import { MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Provider as JotaiProvider } from 'jotai'
 import type { AppProps } from 'next/app'
 
 import { WrapperLayout } from '@/component/layout/WrapperLayout'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient()
+
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <JotaiProvider>
-        <WrapperLayout>
-          <Component {...pageProps} />
-        </WrapperLayout>
-      </JotaiProvider>
+      <QueryClientProvider client={queryClient}>
+        <JotaiProvider>
+          <WrapperLayout>
+            <Component {...pageProps} />
+          </WrapperLayout>
+        </JotaiProvider>
+      </QueryClientProvider>
     </MantineProvider>
   )
 }
