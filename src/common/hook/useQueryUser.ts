bdf685f829d35d7baf13760425409bdad111d9eb
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-17 19:35:41 +0900

    👔 add:認証フロー

--- src/common/hook/useQueryUser.ts
+++ src/common/hook/useQueryUser.ts
@@ -1,23 +1,19 @@
 import type { User } from '@prisma/client'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import { useSetAtom } from 'jotai'
-
-import { isAuthenticatedAtom } from '@/store/auth-atom'
+import { useRouter } from 'next/router'
 
 export const useQueryUser = () => {
-  const setIsAuthenticated = useSetAtom(isAuthenticatedAtom)
-
+  const router = useRouter()
   const getUser = async () => {
     const { data } = await axios.get<Omit<User, 'hashedPassword'>>(`${process.env.NEXT_PUBLIC_API_URL}/user`)
-    setIsAuthenticated(true)
     return data
   }
   return useQuery<Omit<User, 'hashedPassword'>, Error>({
     queryKey: ['user'],
     queryFn: getUser,
     onError: (err: any) => {
-      if (err.response.status === 401 || err.response.status === 403) setIsAuthenticated(false)
+      if (err.response.status === 401 || err.response.status === 403) router.push('/')
     },
   })
 }
