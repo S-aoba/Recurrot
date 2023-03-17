0bdc65dddfd34acef7733566cfa7dd6291dac8f3
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-17 22:04:44 +0900

    👔 add:useQueryQuestions

---
+++ src/common/hook/useQueryQuestions.ts
@@ -0,0 +1,19 @@
+import type { Question } from '@prisma/client'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+export const useQueryQuestions = () => {
+  const router = useRouter()
+  const getQuestions = async () => {
+    const { data } = await axios.get<Question[]>(`${process.env.NEXT_PUBLIC_API_URL}/question`)
+    return data
+  }
+  return useQuery<Question[], Error>({
+    queryKey: ['questions'],
+    queryFn: getQuestions,
+    onError: (err: any) => {
+      if (err.response.status === 401 || err.response.status === 403) router.push('/')
+    },
+  })
+}
