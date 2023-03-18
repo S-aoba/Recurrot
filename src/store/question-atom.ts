cbbdd4f1124084afb62042910f35233ef84072b4
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-18 15:30:16 +0900

    👔 add:editedQuestion

--- src/store/question-atom.ts
+++ src/store/question-atom.ts
@@ -1,6 +1,6 @@
 import { atom } from 'jotai'
 
-import type { MainNavTabStyleType, QuestionType, SubNavTabStyleType } from '@/common/type'
+import type { EditedQuestion, MainNavTabStyleType, QuestionType, SubNavTabStyleType } from '@/common/type'
 
 export const initialQuestion: QuestionType = {
   questionId: '',
@@ -12,10 +12,12 @@ export const initialQuestion: QuestionType = {
   answerList: [],
 }
 
+export const initialEditedQuestion: EditedQuestion = { id: 0, title: '', description: '' }
+
 export const questionAtom = atom<QuestionType>(initialQuestion)
 export const questionListAtom = atom<QuestionType[]>([])
 
 export const mainNavTabStyleAtom = atom<MainNavTabStyleType>('questions')
 export const subNavTabStyleAtom = atom<SubNavTabStyleType>('new-questions')
 
-export const editedQuestionAtom = atom({ id: 0, title: '', description: '' })
+export const editedQuestionAtom = atom<EditedQuestion>(initialEditedQuestion)
