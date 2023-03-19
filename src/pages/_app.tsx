83b33d2bb75dfa00744e71d5378c777bf32668fd
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-19 22:31:06 +0900

    ➖ rm: react-query-devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '../../styles/globals.css'
 
 import { MantineProvider } from '@mantine/core'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import axios from 'axios'
 import { Provider as JotaiProvider } from 'jotai'
 import type { AppProps } from 'next/app'
@@ -45,7 +44,6 @@ export default function App({ Component, pageProps }: AppProps) {
           </WrapperLayout>
         </JotaiProvider>
       </MantineProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
