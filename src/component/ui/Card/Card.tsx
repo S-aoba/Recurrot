8ac67d262be5be96a3b5ffea82c84d3ec905881a
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-22 14:19:54 +0900

    ♻️ 受け取るデータによって表示内容を変更できる形にした

--- src/component/ui/Card/Card.tsx
+++ src/component/ui/Card/Card.tsx
@@ -1,50 +1,48 @@
-import { Avatar, Loader } from '@mantine/core'
+import { Avatar } from '@mantine/core'
+import type { Question } from '@prisma/client'
 import Image from 'next/image'
 
-import { useQueryQuestions } from '@/common/hook/useQueryQuestions'
 import { Link } from '@/lib/next/Link'
 
 /**
  * @package
  */
 
-export const Card = () => {
-  const { data, status } = useQueryQuestions()
-  if (status == 'loading') return <Loader />
-  return (
-    <>
-      {data &&
-        data.map(({ id, createdAt, title, userId }) => {
-          const year = createdAt.toString().slice(0, 4)
-          const month = createdAt.toString().slice(5, 7)
-          const day = createdAt.toString().slice(8, 10)
-          return (
-            <div key={id} className=' col-span-1 h-64 w-80 border border-solid border-gray-300 bg-white'>
-              <Link
-                href={'/dashboard/questions/[id]'}
-                as={`questions/${id}`}
-                className=' absolute block h-64 w-80'
-              ></Link>
-              <div className=' px row-span-2 flex items-center'>
-                <div className=' p-5'>
-                  <Image src={'/typescript.png'} height={95} width={95} alt={'typescript'} className=' rounded-xl' />
-                </div>
-                <div className='w-full p-2'>
-                  <div className=' flex w-full items-center gap-x-3 py-3'>
-                    <Avatar radius='xl' />
-                    <span>userId: {userId}</span>
-                  </div>
-                  <div className=' flex w-full items-center gap-x-3 pb-3'>
-                    <span>投稿日: {`${year} / ${month} / ${day}`}</span>{' '}
-                  </div>
-                </div>
-              </div>
-              <div className=' row-span-4 p-3 text-lg'>
-                <p className=' line-clamp-3'>{title}</p>
-              </div>
+type CardProps = {
+  type: 'question' | 'answer'
+  data: Question
+}
+
+export const Card: React.FC<CardProps> = ({ type, data }) => {
+  if (type == 'question') {
+    const { id, createdAt, title, userId } = data
+    const year = createdAt.toString().slice(0, 4)
+    const month = createdAt.toString().slice(5, 7)
+    const day = createdAt.toString().slice(8, 10)
+
+    return (
+      <div key={id} className=' col-span-1 h-64 w-80 border border-solid border-gray-300 bg-white'>
+        <Link href={'/dashboard/questions/[id]'} as={`questions/${id}`} className=' absolute block h-64 w-80'></Link>
+        <div className=' px row-span-2 flex items-center'>
+          <div className=' p-5'>
+            <Image src={'/typescript.png'} height={95} width={95} alt={'typescript'} className=' rounded-xl' />
+          </div>
+          <div className='w-full p-2'>
+            <div className=' flex w-full items-center gap-x-3 py-3'>
+              <Avatar radius='xl' />
+              <span>userId: {userId}</span>
             </div>
-          )
-        })}
-    </>
-  )
+            <div className=' flex w-full items-center gap-x-3 pb-3'>
+              <span>投稿日: {`${year} / ${month} / ${day}`}</span>{' '}
+            </div>
+          </div>
+        </div>
+        <div className=' row-span-4 p-3 text-lg'>
+          <p className=' line-clamp-3'>{title}</p>
+        </div>
+      </div>
+    )
+  } else {
+    return <div>ANswer</div>
+  }
 }
