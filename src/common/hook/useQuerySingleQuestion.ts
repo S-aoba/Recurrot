6f091f42f8bb5650fe5434b9faaa7cb7a092e0d1
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-22 12:29:34 +0900

    👔 add:質問の個別情報の取得

---
+++ src/common/hook/useQuerySingleQuestion.ts
@@ -0,0 +1,25 @@
+import type { Question } from '@prisma/client'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+export const useQuerySingleQuestions = (id: number) => {
+  const router = useRouter()
+  const getQuestions = async (id: number) => {
+    const res = await axios.get<Question>(`${process.env.NEXT_PUBLIC_API_URL}/question/${id}`, {
+      withCredentials: true,
+    })
+    return res.data
+  }
+  return useQuery<Question, Error>({
+    queryKey: ['singleQuestion', id],
+    queryFn: () => {
+      return getQuestions(id)
+    },
+    enabled: !!id,
+    staleTime: Infinity,
+    onError: (err: any) => {
+      if (err.response.status === 401 || err.response.status === 403) router.push('/')
+    },
+  })
+}
