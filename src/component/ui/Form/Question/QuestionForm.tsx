f8dbea3e04b5d66f26ec4f37429c6a3473564fee
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-23 22:37:41 +0900

    👔 change:投稿と更新で文言変化

--- src/component/ui/Form/Question/QuestionForm.tsx
+++ src/component/ui/Form/Question/QuestionForm.tsx
@@ -16,7 +16,7 @@ export const QuestionForm = () => {
   const [editedQuestion, setEditedQuestion] = useAtom(editedQuestionAtom)
   const [description, setDescription] = useAtom(descriptionAtom)
 
-  const { createQuestionMutation } = useMutateQuestion()
+  const { createQuestionMutation, updateQuestionMutation } = useMutateQuestion()
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -25,13 +25,20 @@ export const QuestionForm = () => {
         title: editedQuestion.title,
         description,
       })
+    else {
+      updateQuestionMutation.mutate({
+        id: editedQuestion.id,
+        title: editedQuestion.title,
+        description,
+      })
+    }
   }
   return (
     <form className=' flex h-full w-11/12 flex-col items-center gap-y-5 py-5' onSubmit={handleSubmit}>
       <Title editedQuestion={editedQuestion} setEditedQuestion={setEditedQuestion} />
       <Content id={editedQuestion.id} description={description} setDescription={setDescription} />
       <Button color='blue' type='submit'>
-        送信
+        {editedQuestion.id === 0 ? '投稿' : '更新'}
       </Button>
     </form>
   )
