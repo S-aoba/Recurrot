208e4d9496e60f24888723111910e727d17be572
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-24 21:53:06 +0900

    🚧 送信後にリセット以外

--- src/component/ui/Form/Answer/AnswerForm.tsx
+++ src/component/ui/Form/Answer/AnswerForm.tsx
@@ -7,12 +7,34 @@ import TextAlign from '@tiptap/extension-text-align'
 import Underline from '@tiptap/extension-underline'
 import { useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import { useAtom, useAtomValue } from 'jotai'
+
+import { useMutateAnswer } from '@/common/hook/useMutateAnswer'
+import { descriptionAtom, editedAnswerAtom } from '@/store/question-atom'
 
 /**
  * @package
  */
 
-export const AnswerForm = () => {
+const escapeHtml = (unsafe: string) => {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+
+type AnswerFormProps = {
+  questionId: number
+}
+
+export const AnswerForm: React.FC<AnswerFormProps> = ({ questionId }) => {
+  const editedAnswer = useAtomValue(editedAnswerAtom)
+  const [description, setDescription] = useAtom(descriptionAtom)
+
+  const { createAnswerMutation } = useMutateAnswer(questionId)
+
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -23,16 +45,29 @@ export const AnswerForm = () => {
       Highlight,
       TextAlign.configure({ types: ['heading', 'paragraph'] }),
     ],
-    content: '',
+    content: editedAnswer.id === 0 ? escapeHtml(description) : description,
+    onUpdate({ editor }) {
+      setDescription(editor.getHTML())
+    },
   })
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (editedAnswer.id === 0) {
+      createAnswerMutation.mutate({
+        description,
+      })
+    }
+  }
+
   return (
-    <form className=' w-full'>
+    <form className=' w-full' onSubmit={handleSubmit}>
       <RichTextEditor editor={editor} className=' h-96 w-full '>
         <RichTextEditor.Content />
       </RichTextEditor>
       <div className=' mt-3 flex justify-end'>
         <Button color='blue' type='submit' className=' hover:transform-none'>
-          投稿
+          {editedAnswer.id === 0 ? '投稿' : '更新'}
         </Button>
       </div>
     </form>
