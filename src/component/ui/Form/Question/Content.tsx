2bbfdaa8c5de1dd11dc621abebbebb126c6e6e59
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-03-25 11:59:32 +0900

    🎨  mv code

--- src/component/ui/Form/Question/Content.tsx
+++ src/component/ui/Form/Question/Content.tsx
@@ -1,49 +1,15 @@
-import { Link, RichTextEditor } from '@mantine/tiptap'
-import Highlight from '@tiptap/extension-highlight'
-import SubScript from '@tiptap/extension-subscript'
-import Superscript from '@tiptap/extension-superscript'
-import TextAlign from '@tiptap/extension-text-align'
-import Underline from '@tiptap/extension-underline'
-import { useEditor } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
-import type { Dispatch, SetStateAction } from 'react'
-
-const escapeHtml = (unsafe: string) => {
-  return unsafe
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#039;')
-}
+import { RichTextEditor } from '@mantine/tiptap'
+import type { Editor } from '@tiptap/react'
 
 /**
  * @package
  */
 
 type ContentProps = {
-  id: number
-  description: string
-  setDescription: Dispatch<SetStateAction<string>>
+  editor: Editor | null
 }
 
-export const Content: React.FC<ContentProps> = ({ id, description, setDescription }) => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Underline,
-      Link,
-      Superscript,
-      SubScript,
-      Highlight,
-      TextAlign.configure({ types: ['heading', 'paragraph'] }),
-    ],
-    content: id === 0 ? escapeHtml(description) : description,
-    onUpdate({ editor }) {
-      setDescription(editor.getHTML())
-    },
-  })
-
+export const Content: React.FC<ContentProps> = ({ editor }) => {
   return (
     <RichTextEditor editor={editor} className=' h-full min-h-fit w-9/12 '>
       <RichTextEditor.Content />
--- src/component/ui/Form/Question/QuestionForm.tsx
+++ src/component/ui/Form/Question/QuestionForm.tsx
@@ -1,9 +1,17 @@
 import { Button } from '@mantine/core'
+import { Link } from '@mantine/tiptap'
+import Highlight from '@tiptap/extension-highlight'
+import SubScript from '@tiptap/extension-subscript'
+import Superscript from '@tiptap/extension-superscript'
+import TextAlign from '@tiptap/extension-text-align'
+import Underline from '@tiptap/extension-underline'
+import { useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
 import { useAtom } from 'jotai'
 import type { FormEvent } from 'react'
 
 import { useMutateQuestion } from '@/common/hook/useMutateQuestion'
-import { descriptionAtom, editedQuestionAtom } from '@/store/question-atom'
+import { editedQuestionAtom, questionDescriptionAtom } from '@/store/question-atom'
 
 import { Content } from './Content'
 import { Title } from './Title'
@@ -12,31 +20,59 @@ import { Title } from './Title'
  * @package
  */
 
+const escapeHtml = (unsafe: string) => {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+
 export const QuestionForm = () => {
   const [editedQuestion, setEditedQuestion] = useAtom(editedQuestionAtom)
-  const [description, setDescription] = useAtom(descriptionAtom)
+  const [description, setDescription] = useAtom(questionDescriptionAtom)
 
   const { createQuestionMutation, updateQuestionMutation } = useMutateQuestion()
 
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Link,
+      Superscript,
+      SubScript,
+      Highlight,
+      TextAlign.configure({ types: ['heading', 'paragraph'] }),
+    ],
+    content: editedQuestion.id !== 0 ? escapeHtml(description) : description,
+    onUpdate({ editor }) {
+      setDescription(editor.getHTML())
+    },
+  })
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    if (editedQuestion.id === 0)
+    if (editedQuestion.id === 0 && editor) {
       createQuestionMutation.mutate({
         title: editedQuestion.title,
         description,
       })
-    else {
+      editor.commands.setContent('')
+    } else if (editedQuestion.id !== 0 && editor) {
       updateQuestionMutation.mutate({
         id: editedQuestion.id,
         title: editedQuestion.title,
         description,
       })
+      editor.commands.setContent('')
     }
   }
+
   return (
     <form className=' flex h-full w-11/12 flex-col items-center gap-y-5 py-5' onSubmit={handleSubmit}>
       <Title editedQuestion={editedQuestion} setEditedQuestion={setEditedQuestion} />
-      <Content id={editedQuestion.id} description={description} setDescription={setDescription} />
+      <Content editor={editor} />
       <Button color='blue' type='submit' className=' hover:transform-none'>
         {editedQuestion.id === 0 ? '投稿' : '更新'}
       </Button>
