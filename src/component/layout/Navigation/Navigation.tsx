ba5c2e443e7404168e2daabdd3ae6de7553961b1
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-04 18:12:51 +0900

    💄 スマホの時にスクロールできるようにした

--- src/component/layout/Navigation/Navigation.tsx
+++ src/component/layout/Navigation/Navigation.tsx
@@ -25,7 +25,7 @@ type NavigationWrapperProps = {
 const NavigationWrapper: React.FC<NavigationWrapperProps> = ({ children }) => {
   return (
     <div className=' flex w-full items-center justify-center border border-t-0 border-r-0 border-l-0 border-solid border-gray-200'>
-      <div className=' flex w-full max-w-[1656px] items-center gap-x-3 px-8'>{children}</div>
+      <div className=' flex w-full max-w-[1656px] items-center gap-x-3 overflow-x-scroll px-8'>{children}</div>
     </div>
   )
 }
