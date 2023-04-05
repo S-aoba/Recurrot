a574a30ba4a084aaba33b57fc08dd667295df4d0
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-05 10:54:01 +0900

    💄 change:最大幅を変更 Headerに合わせた。レイアウトの影響なし

--- src/component/layout/QuestionLayout.tsx
+++ src/component/layout/QuestionLayout.tsx
@@ -5,7 +5,7 @@ type QuestionLayoutProps = {
 export const QuestionLayout: React.FC<QuestionLayoutProps> = ({ children }) => {
   return (
     <main className=' flex h-fit flex-1  justify-center'>
-      <div className=' flex w-full max-w-[1656px] justify-center px-8'>
+      <div className=' flex w-full max-w-[1200px] justify-center px-8'>
         <div className=' grid h-fit grid-cols-1 place-items-center gap-5 py-5 sm:grid-cols-2 sm:place-items-stretch md:grid-cols-3 md:place-items-stretch'>
           {children}
         </div>
