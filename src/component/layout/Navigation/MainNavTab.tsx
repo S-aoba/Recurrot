5b36b22c29e23462a970d115ec89dae6226fc72d
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-07 13:39:10 +0900

    🔥 rm:pageが表示されるタイミングでnavTabが変わるように修正

--- src/component/layout/Navigation/MainNavTab.tsx
+++ src/component/layout/Navigation/MainNavTab.tsx
@@ -1,9 +1,7 @@
-import { useAtom } from 'jotai'
 import Link from 'next/link'
 import type { ReactNode } from 'react'
 
 import type { MainUrlVal } from '@/common/type'
-import { navTabAtom } from '@/store/question-atom'
 
 import { useMainNavTabStyle } from './useMainNavTabStyle'
 
@@ -14,21 +12,16 @@ import { useMainNavTabStyle } from './useMainNavTabStyle'
 type MainNavTabType = {
   href: MainUrlVal
   children: ReactNode
-  handleOnClick: () => void
   className?: string
 }
 
 export const MainNavTab = () => {
   const { questionsStyle } = useMainNavTabStyle()
-  const [navTab, setNavTab] = useAtom(navTabAtom)
 
   const MainNavTabItem: MainNavTabType[] = [
     {
       href: '/dashboard/new-questions',
       children: '質問',
-      handleOnClick: () => {
-        return setNavTab({ ...navTab, main: 'questions', sub: 'new-questions' })
-      },
       className: questionsStyle,
     },
     // 2023/3/21 一旦コメントアウト　後ほど実装
@@ -53,9 +46,9 @@ export const MainNavTab = () => {
   return (
     <>
       {MainNavTabItem &&
-        MainNavTabItem.map(({ href, children, className, handleOnClick }) => {
+        MainNavTabItem.map(({ href, children, className }) => {
           return (
-            <Link key={href} href={href} className={className} onClick={handleOnClick}>
+            <Link key={href} href={href} className={className}>
               {children}
             </Link>
           )
--- src/component/layout/Navigation/SubNavTab.tsx
+++ src/component/layout/Navigation/SubNavTab.tsx
@@ -1,9 +1,7 @@
-import { useAtom } from 'jotai'
 import Link from 'next/link'
 import type { ReactNode } from 'react'
 
 import type { SubUrlVal } from '@/common/type'
-import { navTabAtom } from '@/store/question-atom'
 
 import { useSubNavTabStyle } from './useSubNavTabStyle'
 
@@ -14,7 +12,6 @@ import { useSubNavTabStyle } from './useSubNavTabStyle'
 type SubNavTabType = {
   href: SubUrlVal
   children: ReactNode
-  handleOnClick?: any
   className?: string
 }
 
@@ -22,56 +19,39 @@ export const SubNavTab = () => {
   const { newQuestionsStyle, questionsWaitingAnswerStyle, myQuestionsStyle, myAnswersStyle, myProfileStyle } =
     useSubNavTabStyle()
 
-  const [navTab, setNavTab] = useAtom(navTabAtom)
-
   const SubNavTabItem: SubNavTabType[] = [
     {
       href: '/dashboard/new-questions',
       children: '新着',
-      handleOnClick: () => {
-        return setNavTab({ ...navTab, sub: 'new-questions' })
-      },
       className: newQuestionsStyle,
     },
     {
       href: '/dashboard/question-waiting-answers',
       children: '回答募集中',
-      handleOnClick: () => {
-        return setNavTab({ ...navTab, sub: 'question-waiting-answers' })
-      },
       className: questionsWaitingAnswerStyle,
     },
     {
       href: '/dashboard/my-questions',
       children: '自分の質問',
-      handleOnClick: () => {
-        return setNavTab({ ...navTab, sub: 'my-questions' })
-      },
       className: myQuestionsStyle,
     },
     {
       href: '/dashboard/my-answers',
       children: '自分の回答',
-      handleOnClick: () => {
-        return setNavTab({ ...navTab, sub: 'my-answers' })
-      },
       className: myAnswersStyle,
     },
     {
       href: '/dashboard/my-profile',
       children: 'プロフィール',
-      handleOnClick: () => {
-        return setNavTab({ ...navTab, sub: 'my-profile' })
-      },
       className: myProfileStyle,
     },
   ]
 
   return (
     <>
-      {SubNavTabItem.map(({ href, children, className, handleOnClick }) => {
+      {SubNavTabItem.map(({ href, children, className }) => {
         return (
-          <Link key={href} href={href} className={className} onClick={handleOnClick}>
+          <Link key={href} href={href} className={className}>
             {children}
           </Link>
         )
