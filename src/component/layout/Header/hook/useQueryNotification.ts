c0a5e08e1585f76b1d9e148aab57f87dfd629fdf
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-13 13:07:58 +0900

    🩹 fix:queryKey functionName

--- src/component/layout/Header/hook/useQueryNotification.ts
+++ src/component/layout/Header/hook/useQueryNotification.ts
@@ -6,13 +6,13 @@ import type { Notification } from '@/common/type'
 
 export const useQueryNotification = () => {
   const router = useRouter()
-  const getNotification = async () => {
+  const getNotificationList = async () => {
     const { data } = await axios.get<Notification[]>(`${process.env.NEXT_PUBLIC_API_URL}/notification`)
     return data
   }
   return useQuery<Notification[], Error>({
-    queryKey: ['notifications'],
-    queryFn: getNotification,
+    queryKey: ['notification-list'],
+    queryFn: getNotificationList,
     staleTime: Infinity,
     onError: (err: any) => {
       if (err.response.status === 401 || err.response.status === 403) router.push('/')
