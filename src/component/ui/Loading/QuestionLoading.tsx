09be02a542c573a0e09d24e4cc2888dab4ee6198
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-13 13:37:42 +0900

    💄 fix:UI

--- src/component/ui/Loading/QuestionLoading.tsx
+++ src/component/ui/Loading/QuestionLoading.tsx
@@ -13,26 +13,26 @@ export const QuestionLoading = () => {
           >
             <div className=' flex h-2/4 justify-between pl-2'>
               <div className=' flex h-full items-center'>
-                <Skeleton height={50} width={50} />
+                <Skeleton height={50} width={50} circle />
               </div>
               <div className='flex w-full flex-col px-4 py-2'>
                 <div className=' flex w-full items-center gap-x-3 py-3'>
-                  <Skeleton height={30} width={30} />
-                  <Skeleton height={5} />
+                  <Skeleton height={30} width={30} circle />
+                  <Skeleton height={5} width={100} />
                 </div>
-                <div className=' flex w-full gap-x-3 p-2 text-sm '>
-                  <Skeleton height={5} />
-                  <div className=' flex gap-x-1'>
-                    <Skeleton height={10} width={10} circle />
-                    <Skeleton height={10} width={10} />
+                <div className=' flex w-full items-center gap-x-3 p-2 text-sm '>
+                  <Skeleton height={5} width={100} />
+                  <div className=' flex items-center gap-x-1'>
+                    <Skeleton height={15} width={10} circle animate={false} />
+                    <Skeleton height={10} width={20} />
                   </div>
                 </div>
               </div>
             </div>
-            <div className=' flex h-2/4 flex-col items-center justify-center gap-y-3 rounded-2xl bg-[#1976d2] px-2'>
-              <Skeleton height={10} className=' w-11/12' />
-              <Skeleton height={10} className=' w-11/12' />
-              <Skeleton height={10} className=' w-11/12' />
+            <div className=' flex h-2/4 flex-col items-start justify-center gap-y-3 rounded-2xl bg-[#1976d2] px-2'>
+              <Skeleton height={10} width={100} />
+              <Skeleton height={10} width={150} />
+              <Skeleton height={10} width={200} />
             </div>
           </div>
         )
