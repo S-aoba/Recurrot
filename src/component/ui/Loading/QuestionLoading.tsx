b165e99ea9a62d5ca40109620f7ce109fb67f72c
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-20 20:44:28 +0900

    💄 🐛 loadingの表示位置の修正とUIの変更

--- src/component/ui/Loading/QuestionLoading.tsx
+++ src/component/ui/Loading/QuestionLoading.tsx
@@ -1,11 +1,9 @@
-import { Loader } from '@mantine/core'
-
-import { QuestionLayout } from '@/component/layout/QuestionLayout'
-
 export const QuestionLoading = () => {
   return (
-    <QuestionLayout>
-      <Loader />
-    </QuestionLayout>
+    <main className=' flex h-fit flex-1 justify-center bg-[#fafafa] py-3'>
+      <div className='flex justify-center'>
+        <div className='h-8 w-8 animate-spin rounded-lg bg-blue-300'></div>
+      </div>
+    </main>
   )
 }
