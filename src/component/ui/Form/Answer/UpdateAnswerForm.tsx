9f4a062e96d77550d301c79c0230acc4d5624a18
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-20 14:57:12 +0900

    ♻️ Update時の挙動の更新

--- src/component/ui/Form/Answer/UpdateAnswerForm.tsx
+++ src/component/ui/Form/Answer/UpdateAnswerForm.tsx
@@ -1,15 +1,14 @@
 import { Button } from '@mantine/core'
-import { useDisclosure } from '@mantine/hooks'
 import { RichTextEditor } from '@mantine/tiptap'
 import { useAtom, useAtomValue } from 'jotai'
-import type { Dispatch, SetStateAction } from 'react'
+import type { Dispatch, FormEvent, SetStateAction } from 'react'
+import { useState } from 'react'
 
 import { useAnswerForm } from '@/common/hook/useAnswerForm'
 import { useMutateAnswer } from '@/common/hook/useMutateAnswer'
 import { answerDescriptionAtom, editedAnswerAtom } from '@/store/atom'
 
 import { useDescriptionEditor } from '../../../../common/hook/useDescriptionEditor'
-import { Modal } from '../../Modal'
 
 /**
  * @package
@@ -21,55 +20,51 @@ type AnswerFormProps = {
 }
 
 export const UpdateAnswerForm: React.FC<AnswerFormProps> = ({ questionId, setIsEdit }) => {
-  const [isOpened, { open: handleOpen, close: handleClose }] = useDisclosure(false)
+  const [isLoading, setIsLoading] = useState(false)
 
-  const [editedAnswer, setEditedAnswer] = useAtom(editedAnswerAtom)
+  const [editedAnswer, _] = useAtom(editedAnswerAtom)
   const description = useAtomValue(answerDescriptionAtom)
 
   const { updateAnswerMutation } = useMutateAnswer(questionId)
 
   const { answerEditor } = useDescriptionEditor()
 
-  const handleSubmit = () => {
-    if (editedAnswer.id !== '0' && answerEditor && setIsEdit) {
-      setIsEdit(false)
-      updateAnswerMutation.mutate({
-        id: editedAnswer.id,
-        description,
-      })
-      answerEditor.commands.setContent('')
-    }
-    setEditedAnswer({ ...editedAnswer, id: '0' })
-    handleClose()
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // 1秒後にupdateAnswerMutationを実行
+    setIsLoading(true)
+    setTimeout(() => {
+      if (editedAnswer.id !== '0' && answerEditor && setIsEdit) {
+        updateAnswerMutation.mutate({
+          id: editedAnswer.id,
+          description,
+        })
+        answerEditor.commands.setContent('')
+        setIsEdit(false)
+        setIsLoading(false)
+      }
+    }, 1000)
   }
   useAnswerForm()
 
   return (
     <>
-      <Modal
-        opened={isOpened}
-        onClose={handleClose}
-        onSubmit={handleSubmit}
-        buttonWord='更新する'
-        modalTitle='回答を更新する'
-      />
-
-      <div className=' w-full'>
+      <form className=' w-full' onSubmit={handleSubmit}>
         <RichTextEditor editor={answerEditor} className=' h-96 w-full rounded-md bg-white'>
           <RichTextEditor.Content />
         </RichTextEditor>
         <div className=' mt-3 flex justify-end'>
           <Button
             color='blue'
-            type='button'
-            onClick={handleOpen}
+            type='submit'
             className=' hover:transform-none'
             disabled={description === ''}
+            loading={isLoading}
           >
-            更新する
+            {isLoading ? '更新中です' : '更新する'}
           </Button>
         </div>
-      </div>
+      </form>
     </>
   )
 }
