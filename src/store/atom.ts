2583b9fd4f65ed286155202aaaf5b3632949aabb
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-23 19:28:37 +0900

    ⚰️

--- src/store/atom.ts
+++ src/store/atom.ts
@@ -21,8 +21,6 @@ export const resetAnswerDescriptionAtom = atom(null, (_, set) => {
 const initialEditedAnswer: EditedAnswer = { id: '0', description: '' }
 export const editedAnswerAtom = atom<EditedAnswer>(initialEditedAnswer)
 
-// export const navTabAtom = atom<NavTab>({ main: null, sub: null })
-
 // editedQuestionAtom questionDescriptionAtomのすべての値がセットされたらfalseを返す
 export const isQuestionDisabledAtom = atom((get) => {
   const { title, hashtags } = get(editedQuestionAtom)
