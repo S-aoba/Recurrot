dcc8917b1abb0a8b3bbe462cae561885be21293c
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-25 19:19:22 +0900

    🐛 元に戻す

--- src/pages/dashboard/questions/[id]/index.tsx
+++ src/pages/dashboard/questions/[id]/index.tsx
@@ -1,33 +1,14 @@
-import axios from 'axios'
-import type { GetServerSideProps, NextPage } from 'next'
+import type { GetServerSideProps } from 'next'
 
-import type { SingleQuestion } from '@/common/type'
 import { QuestionDetailPage } from '@/component/page/[id]'
 
-type QuestionDetailProps = {
-  question: SingleQuestion
+const QuestionDetail = () => {
+  return <QuestionDetailPage />
 }
 
-const QuestionDetail: NextPage<QuestionDetailProps> = ({ question }) => {
-  return <QuestionDetailPage question={question} />
-}
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookie = ctx.req.headers.cookie
-
-  const res = await axios.get<SingleQuestion>(
-    `${process.env.NEXT_PUBLIC_FRONTEND_URL || process.env.NEXT_PUBLIC_VERCEL_URL}/api/question/${ctx.query.id}`,
-    {
-      headers: {
-        cookie,
-      },
-    }
-  )
-  const question = res.data
-
+export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
-      question,
       layout: 'WrapperLayout',
     },
   }
