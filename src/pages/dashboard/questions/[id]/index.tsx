a157bcc61e5416f065f3c11bed995d23f32add3b
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-25 22:17:10 +0900

    🚧 SSRでdataが取得できているかどうかテスト

--- src/pages/dashboard/questions/[id]/index.tsx
+++ src/pages/dashboard/questions/[id]/index.tsx
@@ -1,17 +1,44 @@
-import type { GetServerSideProps } from 'next'
+import axios from 'axios'
+import type { GetServerSideProps, NextPage } from 'next'
+import { parseCookies } from 'nookies'
 
+import type { SingleQuestion } from '@/common/type'
 import { QuestionDetailPage } from '@/component/page/[id]'
 
-const QuestionDetail = () => {
-  return <QuestionDetailPage />
+type Props = {
+  question: SingleQuestion
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+const QuestionDetail: NextPage<Props> = ({ question }) => {
+  return <QuestionDetailPage question={question} />
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const id = ctx.query.id
+  const cookies = parseCookies(ctx)
+  const cookie = Object.keys(cookies)
+    .map((cookieName) => {
+      return `${cookieName}=${cookies[cookieName]}`
+    })
+    .join(';')
+
+  const data = await getSingleQuestion(id, cookie)
+
   return {
     props: {
+      question: data,
       layout: 'WrapperLayout',
     },
   }
 }
 
 export default QuestionDetail
+
+const getSingleQuestion = async (id: string | string[] | undefined, cookie: string) => {
+  const res = await axios.get<SingleQuestion>(`${process.env.NEXT_PUBLIC_API_URL}/question/${id}`, {
+    headers: {
+      cookie,
+    },
+  })
+  return res.data
+}
