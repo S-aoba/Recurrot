015fed8add130c8329b667cada9ce0b955bcd401
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-26 11:01:38 +0900

    🚧

--- src/pages/dashboard/questions/[id]/index.tsx
+++ src/pages/dashboard/questions/[id]/index.tsx
@@ -1,17 +1,46 @@
+// import axios from 'axios'
 import type { GetServerSideProps } from 'next'
+import nookies from 'nookies'
 
+// import type { SingleQuestion } from '@/common/type'
 import { QuestionDetailPage } from '@/component/page/[id]'
 
-const QuestionDetail = () => {
-  return <QuestionDetailPage />
+const QuestionDetail = ({ cookie }: any) => {
+  // console.log('data', data)
+  return (
+    <div>
+      <p>{cookie}</p>
+      <QuestionDetailPage />
+    </div>
+  )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  // const { id } = ctx.query
+  const cookies = nookies.get(ctx)
+  // cookiesをstringに変換して、cookieに入れる
+  const cookie = Object.keys(cookies).reduce((acc, cur) => {
+    return `${acc}${cur}=${cookies[cur]};`
+  }, '')
+
+  // const data = await getSingleQuestion(id, cookie)
+
   return {
     props: {
+      // data,
+      cookie,
       layout: 'WrapperLayout',
     },
   }
 }
+
+// const getSingleQuestion = async (id, cookie) => {
+//   const res = await axios.get<SingleQuestion>(`${process.env.NEXT_PUBLIC_API_URL}/question/${id}`, {
+//     headers: {
+//       cookie,
+//     },
+//   })
+//   return res.data
+// }
 
 export default QuestionDetail
