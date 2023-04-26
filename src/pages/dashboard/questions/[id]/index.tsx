4eb067ccb95c47d8dfee28f204edcca7c9fb5d92
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-26 13:10:00 +0900

    🩹

--- src/pages/dashboard/questions/[id]/index.tsx
+++ src/pages/dashboard/questions/[id]/index.tsx
@@ -5,18 +5,24 @@ import nookies from 'nookies'
 // import type { SingleQuestion } from '@/common/type'
 import { QuestionDetailPage } from '@/component/page/[id]'
 
-const QuestionDetail = ({ cookie }: any) => {
+const QuestionDetail = ({ id, cookie2, req, cookie }: any) => {
   // console.log('data', data)
   return (
     <div>
       <p>{cookie}</p>
+      <p>{id}</p>
+      <p>{cookie2}</p>
+      <p>{req}</p>
+
       <QuestionDetailPage />
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  // const { id } = ctx.query
+  const { id } = ctx.query
+  const cookie2 = ctx.req.headers.cookie
+  const req = ctx.req
   const cookies = nookies.get(ctx)
   // cookiesをstringに変換して、cookieに入れる
   const cookie = Object.keys(cookies).reduce((acc, cur) => {
@@ -27,6 +33,9 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
   return {
     props: {
+      id,
+      cookie2,
+      req,
       // data,
       cookie,
       layout: 'WrapperLayout',
