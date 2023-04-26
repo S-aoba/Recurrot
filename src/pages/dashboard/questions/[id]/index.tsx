ec5bb13e8df17834c2b6e883fb835c9df5d2733f
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-26 15:23:01 +0900

    🔥 nookiesの削除

--- src/pages/dashboard/questions/[id]/index.tsx
+++ src/pages/dashboard/questions/[id]/index.tsx
@@ -1,51 +1,17 @@
-// import axios from 'axios'
 import type { GetServerSideProps } from 'next'
-import nookies from 'nookies'
 
-// import type { SingleQuestion } from '@/common/type'
 import { QuestionDetailPage } from '@/component/page/[id]'
 
-const QuestionDetail = ({ cookie }: any) => {
-  // console.log('data', data)
-  return (
-    <div>
-      <p>{cookie}</p>
-      <QuestionDetailPage />
-    </div>
-  )
+const QuestionDetail = () => {
+  return <QuestionDetailPage />
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { id } = ctx.query
-  const cookie2 = ctx.req.headers.cookie
-  // const req = ctx.req
-  const cookies = nookies.get(ctx)
-  // cookiesをstringに変換して、cookieに入れる
-  const cookie = Object.keys(cookies).reduce((acc, cur) => {
-    return `${acc}${cur}=${cookies[cur]};`
-  }, '')
-
-  // const data = await getSingleQuestion(id, cookie)
-
+export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
-      id,
-      cookie2,
-      // req,
-      // data,
-      cookie,
       layout: 'WrapperLayout',
     },
   }
 }
-
-// const getSingleQuestion = async (id, cookie) => {
-//   const res = await axios.get<SingleQuestion>(`${process.env.NEXT_PUBLIC_API_URL}/question/${id}`, {
-//     headers: {
-//       cookie,
-//     },
-//   })
-//   return res.data
-// }
 
 export default QuestionDetail
