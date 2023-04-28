bbdc029c1aa4431830ee3a699df86acf4cd09a02
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-28 13:03:35 +0900

    🚧

--- src/pages/dashboard/questions/[id]/index.tsx
+++ src/pages/dashboard/questions/[id]/index.tsx
@@ -1,17 +1,46 @@
-import type { GetServerSideProps } from 'next'
+import axios from 'axios'
+import type { GetServerSideProps, NextPage } from 'next'
 
+import type { SingleQuestion } from '@/common/type'
 import { QuestionDetailPage } from '@/component/page/[id]'
 
-const QuestionDetail = () => {
-  return <QuestionDetailPage />
+type Props = {
+  question: SingleQuestion
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+const QuestionDetail: NextPage<Props> = ({ question }) => {
+  return (
+    <>
+      <div>{question.title}</div>
+      <QuestionDetailPage />
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const csrfToken = ctx.req.cookies._csrf
+  const accessToken = ctx.req.cookies.access_token
+
+  // console.log(`_csrf=${csrfToken}; access_token=${accessToken}}`)
+  const id = ctx.query.id
+  const cookie = `_csrf=${csrfToken}; access_token=${accessToken}`
+  const question = await getSingleQuestion(id, cookie)
+
   return {
     props: {
+      question,
       layout: 'WrapperLayout',
     },
   }
 }
 
 export default QuestionDetail
+
+const getSingleQuestion = async (id: string | string[] | undefined, cookie: string) => {
+  const res = await axios.get<SingleQuestion>(`${process.env.NEXT_PUBLIC_API_URL}/question/${id}`, {
+    headers: {
+      cookie,
+    },
+  })
+  return res.data
+}
