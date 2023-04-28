54edb5208f78e2fbaff4535be1161f6978b48924
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-28 15:29:45 +0900

    💄 問題を指定している場合のどの問題かを表示するUIの追加 プロトタイプ

--- src/component/ui/Card/Card.tsx
+++ src/component/ui/Card/Card.tsx
@@ -1,6 +1,7 @@
 import { Avatar } from '@mantine/core'
 import { IconMessageDots } from '@tabler/icons-react'
 import { motion } from 'framer-motion'
+import type { NextPage } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -29,33 +30,27 @@ export const Card: React.FC<CardProps> = ({ question, index }) => {
     >
       <div
         key={id}
-        className=' relative col-span-1 box-content flex h-60 w-60 flex-col gap-y-2 rounded-3xl border border-solid border-gray-200 bg-white p-2 shadow-md duration-500 hover:relative hover:translate-x-2 hover:-translate-y-2 hover:cursor-pointer hover:shadow-[-8px_8px_0px_0px_#171717]'
+        className=' relative col-span-1 box-content flex h-fit w-60 flex-col gap-y-2 rounded-3xl border border-solid border-gray-200 bg-white p-2 shadow-md duration-500 hover:relative hover:translate-x-2 hover:-translate-y-2 hover:cursor-pointer hover:shadow-[-8px_8px_0px_0px_#171717]'
       >
-        <Link href={`/dashboard/questions/${id}`} className=' absolute top-0 left-0 h-full w-full rounded-2xl'></Link>
-        <div className=' flex justify-between pl-2'>
-          <div className=' flex h-full items-center'>
-            <Image
-              src={`/langIcon/${question.hashtags[0]}.svg`}
-              height={40}
-              width={40}
-              alt={question.hashtags[0] == 'csharp' ? 'C#' : `${question.hashtags[0]}`}
-              className=' rounded-xl'
-              priority
-            />
-          </div>
-          <div className='flex w-full flex-col px-4 py-2'>
-            <div className=' flex w-full items-center gap-x-2 py-3'>
-              <Avatar
-                src={question.user.profileImage}
-                radius='xl'
-                size={'sm'}
-                className=' border border-solid border-gray-200 shadow-sm'
-              />
-              <span className=' text-sm text-gray-500 line-clamp-1'>
-                {user.userName === null ? '名無しユーザー' : user.userName}
-              </span>
-            </div>
-            <div className=' flex w-full flex-col gap-x-3 p-2 text-sm text-gray-500'>
+        <Link
+          href={`/dashboard/questions/${id}`}
+          className=' absolute top-0 left-0 z-20 h-full w-full rounded-2xl'
+        ></Link>
+        <div className=' flex flex-col'>
+          <Image
+            src={`/langIcon/${question.hashtags[0]}.svg`}
+            height={40}
+            width={40}
+            alt={question.hashtags[0] == 'csharp' ? 'C#' : `${question.hashtags[0]}`}
+            className=' absolute -top-3 -left-3 z-10 rounded-xl'
+            priority
+          />
+          <span className=' absolute -top-3 right-0 z-10 w-10/12 rounded-lg bg-[#000080] p-1 text-sm text-white line-clamp-1'>
+            問題1:メートルをマイルに変換する
+          </span>
+          <div className='flex w-full flex-col gap-y-2 pt-5 pl-6'>
+            <AvatarUserName profileImage={question.user.profileImage} userName={user.userName} />
+            <div className=' flex w-full gap-x-3 text-sm text-gray-500'>
               <span>投稿日: {`${year}/${month}/${day}`}</span>
               <div className=' flex items-center gap-x-1'>
                 <IconMessageDots size={18} />
@@ -76,8 +71,22 @@ type TitleProps = {
 
 const Title: React.FC<TitleProps> = ({ title }) => {
   return (
-    <div className=' flex h-32 items-center justify-center rounded-2xl bg-mainColor px-2'>
-      <p className='text-sm text-white line-clamp-3'>{title}</p>
+    <div className=' flex h-20 items-center justify-center rounded-2xl bg-mainColor px-2'>
+      <p className=' mt-0 mb-0 text-sm text-white line-clamp-3'>{title}</p>
+    </div>
+  )
+}
+
+type AvatarAndUserNameProps = {
+  profileImage: string | null
+  userName: string | null
+}
+
+const AvatarUserName: NextPage<AvatarAndUserNameProps> = ({ profileImage, userName }) => {
+  return (
+    <div className=' flex w-full items-center gap-x-2'>
+      <Avatar src={profileImage} radius='xl' size={'sm'} className=' border border-solid border-gray-200 shadow-sm' />
+      <span className=' text-sm text-gray-500 line-clamp-1'>{userName === null ? '名無しユーザー' : userName}</span>
     </div>
   )
 }
