35e4bf0ba6a3c8fc7571660ec796f9c4a636f3be
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-04-28 15:28:50 +0900

    💄 ♻️ fix:UI

--- src/component/layout/QuestionLayout.tsx
+++ src/component/layout/QuestionLayout.tsx
@@ -6,7 +6,7 @@ export const QuestionLayout: React.FC<QuestionLayoutProps> = ({ children }) => {
   return (
     <main className=' flex h-fit flex-1 justify-center bg-[#fafafa]'>
       <div className=' flex w-full max-w-[1200px] justify-center px-8'>
-        <div className=' grid h-fit w-full max-w-[850px] grid-cols-1 place-items-center gap-5 py-5 xs:grid-cols-2 sm:grid-cols-3'>
+        <div className=' grid h-fit w-full max-w-[850px] grid-cols-1 place-items-center gap-x-5 gap-y-8 py-10 xs:grid-cols-2 sm:grid-cols-3'>
           {children}
         </div>
       </div>
